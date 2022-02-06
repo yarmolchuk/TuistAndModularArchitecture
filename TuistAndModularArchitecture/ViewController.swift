a5257edc81f72a0adcb67457294b6ae2d3efaabb
Dima Yarmolchuk <yarmolchuk@gmail.com>
2022-02-06 14:27:58 +0200

    first commit

---
+++ TuistAndModularArchitecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TuistAndModularArchitecture
+//
+//  Created by Dima Yarmolchuk on 2/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
